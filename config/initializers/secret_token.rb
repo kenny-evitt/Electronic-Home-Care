5bd4ad24c9d0a186f456e2cbb2a62a85f63d60e1
unknown <Kenny@.(none)>
2012-02-11 19:44:16 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Electronichomecare::Application.config.secret_token = '4bb53a1083811bd16f7bfa96bf9c93082206db9b3a903dd6b924d53dad10725b1cdf1dd04fb12e126930932e1cb5621aed193191aa35858377587b9eb1af171c'
